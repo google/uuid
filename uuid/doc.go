42c4e65571c8e5052ed59f5ded1e317f33975bb4
Paul Borman <borman@google.com>
2011-08-08 21:13:39 -0700

    uuid: add doc.go file
    R=paul
    CC=borman
    http://codereview.appspot.com/4856045

---
+++ uuid/doc.go
@@ -0,0 +1,7 @@
+// Copyright 2011 Google Inc.  All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// The uuid package generates and inspects UUIDs based on RFC 4122 and
+// DCE 1.1: Authentication and Security Services.
+package uuid
