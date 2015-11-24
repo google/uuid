26c42f1335f80f7b66dfec20af4c42bcbfbb3594
David E. Wheeler <david@justatheory.com>
2015-11-24 15:42:06 -0800

    Value() is on UUID, not *UUID.

--- sql.go
+++ sql.go
@@ -61,6 +61,6 @@ func (uuid *UUID) Scan(src interface{}) error {
 // Value implements sql.Valuer so that UUIDs can be written to databases
 // transparently. Currently, UUIDs map map to strings. Please consult
 // database-specific driver documentation for matching types.
-func (uuid *UUID) Value() (driver.Value, error) {
+func (uuid UUID) Value() (driver.Value, error) {
 	return uuid.String(), nil
 }
