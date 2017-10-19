54117dbb389177009855a553e6e78bc65aba5bb3
kortschak <dan.kortschak@adelaide.edu.au>
2017-10-19 14:04:08 +1030

    Remove never-true conditional

--- node.go
+++ node.go
@@ -94,9 +94,6 @@ func SetNodeID(id []byte) bool {
 // NodeID returns the 6 byte node id encoded in uuid.  It returns nil if uuid is
 // not valid.  The NodeID is only well defined for version 1 and 2 UUIDs.
 func (uuid UUID) NodeID() []byte {
-	if len(uuid) != 16 {
-		return nil
-	}
 	var node [6]byte
 	copy(node[:], uuid[10:])
 	return node[:]
