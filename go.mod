c51466134a9f11d0a6863f83111ddd0680f72250
inliquid <inliquid@users.noreply.github.com>
2019-01-03 17:48:40 +0300

    - Implemented bson.Getter/Setter interfaces for correct marshaling/unmarshaling of MongoDB's BSON binary UUID format with new (recommended) UUID as default kind (0x04). - Introduced SetBSONKind function to make user able to set different BSON kind.

--- go.mod
+++ go.mod
@@ -1,1 +1,3 @@
 module github.com/google/uuid
+
+require gopkg.in/mgo.v2 v2.0.0-20180705113604-9856a29383ce
--- marshal.go
+++ marshal.go
@@ -4,7 +4,19 @@
 
 package uuid
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+
+	"gopkg.in/mgo.v2/bson"
+)
+
+var (
+	// By default, new (recommended) UUID subtype/kind (0x04) is used, see
+	// https://studio3t.com/knowledge-base/articles/mongodb-best-practices-uuid-data/#binary-subtypes-0x03-and-0x04 and
+	// http://bsonspec.org/spec.html for details. Can be changed with SetBSONKind.
+	bsonKind byte = 0x04
+)
 
 // MarshalText implements encoding.TextMarshaler.
 func (uuid UUID) MarshalText() ([]byte, error) {
@@ -33,5 +45,43 @@ func (uuid *UUID) UnmarshalBinary(data []byte) error {
 		return fmt.Errorf("invalid UUID (got %d bytes)", len(data))
 	}
 	copy(uuid[:], data)
+	return nil
+}
+
+// GetBSON implements bson.Getter for marshaling UUID in BSON binary UUID format.
+// By default Kind of 0x04 (new UUID) will be used. Can be changed with SetBSONKind.
+func (uuid UUID) GetBSON() (interface{}, error) {
+	toMarshal := bson.Binary{
+		Kind: bsonKind,
+		Data: uuid[:],
+	}
+
+	return toMarshal, nil
+}
+
+// SetBSON implements bson.Setter for unmarshaling UUID from BSON binary UUID format.
+// By default Kind of 0x04 (new UUID) will be used. Can be changed with SetBSONKind.
+func (uuid *UUID) SetBSON(raw bson.Raw) error {
+	var toUnmarshal bson.Binary
+
+	err := raw.Unmarshal(&toUnmarshal)
+	if err != nil {
+		return err
+	}
+
+	*uuid, err = FromBytes(toUnmarshal.Data)
+
+	return err
+}
+
+// SetBSONKind changes BSON UUID Kind which will be used by GetBSON and SetBSON. Only values of
+// 0x03 (Legacy UUID) or 0x04 (new UUID) can be used, SetBSONKind returns error when requested kind is different.
+func SetBSONKind(kind byte) error {
+	if kind < 0x03 || kind > 0x04 {
+		return errors.New("requested BSON UUID kind is not allowed")
+	}
+
+	bsonKind = kind
+
 	return nil
 }
