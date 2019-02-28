63b66ddfe0ddd512d239ead433bc6124ae02a56b
talis <talis@il.ibm.com>
2019-02-28 14:59:24 +0200

    reused version4 NewRandom() and added better testing

--- uuid_source_test.go
+++ uuid_source_test.go
@@ -7,23 +7,53 @@ import (
 )
 
 func TestUuidSources(t *testing.T) {
+	
+	// Two identical sources, should give same sequence
 	currentTime := time.Now().UnixNano()
 	uuidSourceA := NewSource(rand.New(rand.NewSource(currentTime)))
 	uuidSourceB := NewSource(rand.New(rand.NewSource(currentTime)))
 	
 	for i := 0; i < 10; i++ {
-		if uuidSourceA.New().String() != uuidSourceB.New().String() {
-			t.Error("Uuid values are not reproducaible!")
+		uuid1 := uuidSourceA.New()
+		uuid2 := uuidSourceB.New()
+		if uuid1 != uuid2 {
+			t.Errorf("expected duplicates, got %q and %q", uuid1, uuid2)
 		}
 	}
 	
-	uuidSourceA = NewSource(rand.New(rand.NewSource(123)))
-	uuidSourceB = NewSource(rand.New(rand.NewSource(456)))
-	
+	// Set rander with nil, each source will be random
+	uuidSourceA.SetRand(nil)
+	uuidSourceB.SetRand(nil)
 
 	for i := 0; i < 10; i++ {
-		if uuidSourceA.New().String() == uuidSourceB.New().String() {
-			t.Error("Uuid values should not match!")
+		uuid1 := uuidSourceA.New()
+		uuid2 := uuidSourceB.New()
+		if uuid1 == uuid2 {
+		t.Errorf("unexpected duplicates, got %q", uuid1)
+		}
+	}
+	
+	// Set rander to rand source with same seed, should give same sequence
+	uuidSourceA.SetRand(rand.New(rand.NewSource(123)))
+	uuidSourceB.SetRand(rand.New(rand.NewSource(123)))
+
+	for i := 0; i < 10; i++ {
+		uuid1 := uuidSourceA.New()
+		uuid2 := uuidSourceB.New()
+		if uuid1 != uuid2 {
+			t.Errorf("expected duplicates, got %q and %q", uuid1, uuid2)
+		}
+	}
+
+	// Set rander to rand source with different seeds, should not give same sequence
+	uuidSourceA.SetRand(rand.New(rand.NewSource(456)))
+	uuidSourceB.SetRand(rand.New(rand.NewSource(789)))
+
+	for i := 0; i < 10; i++ {
+		uuid1 := uuidSourceA.New()
+		uuid2 := uuidSourceB.New()
+		if uuid1 == uuid2 {
+		t.Errorf("unexpected duplicates, got %q", uuid1)
 		}
 	}
 
