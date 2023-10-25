997a025b6ae86f9fba8ef51429cca0c044f13040
Dylan Bargteil <dbargteil@niche.com>
2023-10-25 11:53:01 -0400

    docs: improve comments on UUIDs

--- uuid.go
+++ uuid.go
@@ -295,10 +295,10 @@ func DisableRandPool() {
 	poolPos = randPoolSize
 }
 
-// UUIDs is a slice of UUID types
+// UUIDs is a slice of UUID types.
 type UUIDs []UUID
 
-// Strings returns a slice of the string form of each UUID in the received slice of UUIDs
+// Strings returns a string slice containing the string form of each UUID in uuids.
 func (uuids UUIDs) Strings() []string {
 	var uuidStrs = make([]string, len(uuids))
 	for i, uuid := range uuids {
