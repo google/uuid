1a3d1fec153ec51994d0ec4993377bf7135bad8c
Dylan Bargteil <dbargteil@niche.com>
2023-10-26 10:07:35 -0400

    feat: UUIDs slice type with Strings() convenience method (#133)
    * feat: add uuid slice type with strings convenience method
    
    * test: benchmark new UUIDs.Strings() feature
    
    * docs: improve comments on UUIDs
    
    * fix: typos in UUIDs strings benchmark

--- uuid.go
+++ uuid.go
@@ -294,3 +294,15 @@ func DisableRandPool() {
 	poolMu.Lock()
 	poolPos = randPoolSize
 }
+
+// UUIDs is a slice of UUID types.
+type UUIDs []UUID
+
+// Strings returns a string slice containing the string form of each UUID in uuids.
+func (uuids UUIDs) Strings() []string {
+	var uuidStrs = make([]string, len(uuids))
+	for i, uuid := range uuids {
+		uuidStrs[i] = uuid.String()
+	}
+	return uuidStrs
+}
