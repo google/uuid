489959a18655ac4256bd2b2ad0029970e5ffc8a0
KotaroYamazaki <sentoryoku53m@gmail.com>
2024-02-22 11:36:50 +0900

    use MustParse(...)  inseted of Must(Parse(...))

--- json_test.go
+++ json_test.go
@@ -10,7 +10,7 @@ import (
 	"testing"
 )
 
-var testUUID = Must(Parse("f47ac10b-58cc-0372-8567-0e02b2c3d479"))
+var testUUID = MustParse("f47ac10b-58cc-0372-8567-0e02b2c3d479")
 
 func TestJSON(t *testing.T) {
 	type S struct {
